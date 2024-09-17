d6ce3b5623b3d55ed0019021806a1723c69bcce4
Murilo <m.coelhos1212@gmail.com>
2024-09-17 17:31:10 -0300

    add ci.yml file

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "http://localhost:5173",
+    env: {
+      API_URL: "http://localhost:3333/"
+    }
   },
 });
